28e6c3e00d30a17f2f36c0c76d56309267e22b09
PrAYtele <rui.hou@wayz.ai>
2019-07-16 15:41:40 +0800

    整理了数据导入工具

--- proj_wayz/lib/sql.py
+++
@@ -1,8 +0,0 @@
-import psycopg2
-def postgre():
-    conn = psycopg2.connect(dbname="py_brand_wgds_0709",
-                user="postgres",
-                password="wayzpg@1234",
-                host="172.3.0.107",
-                port="5432")
-    return conn
